d9d5ae2d97e121efeb77f6635eabee963a260fd9
sacul29 <lucasch@gwu.edu>
2014-12-01 06:51:07 -0500

    Added ability to change settings

--- commenting_functionality/core.js
+++ commenting_functionality/core.js
@@ -1,12 +1,7 @@
-var bubbleDOM = document.createElement('div');
-bubbleDOM.setAttribute('class', 'selection_bubble');
-document.body.appendChild(bubbleDOM);
 
-
-
+restoreColors();
 
 document.addEventListener('mousedown', function (e) {
-  bubbleDOM.style.visibility = 'hidden';
 }, false);
 
 var flag = false;
@@ -15,7 +10,9 @@ var comText = "";
 var commentNum = 0;
 var app_ids = 1;
 var savedDivs = [];
-var pageData = {};
+//var pageData = {};
+var boxColor = "#fcc";
+var highColor = "yellow";
 //mousemove
 
 // Mouse listener for any move event on the current document.
@@ -34,37 +31,37 @@ document.addEventListener('mouseup', function (e) {
 
 function replaceText(selectedText,selectedNode,range)
 {
-  console.log(selectedText);
+  //console.log(selectedText);
+
+  var currentText = range.cloneContents();  
   //get rid of current contents.
   range.deleteContents();
   //add the subscript tag
   var sup = document.createElement("sup");
+  sup.style.color = "red";
   //add the number to be subscripted
-  var newText = document.createTextNode(commentNum);
+  var newText = document.createTextNode("("+commentNum+")");
   
   //creates an element to change the text color.
   var colored = document.createElement("font");
   colored.style.color = "red";
 
   //create a text node for the old text
-  var oldText = document.createTextNode(selectedText);
-  colored.appendChild(oldText);
-  oldText.color = "red";
+  //var oldText = document.createTextNode(selectedText);
+  colored.appendChild(currentText);
+  //oldText.color = "red";
 
   //add the comment number to the subscript tag
   sup.appendChild(newText);
 
-  //adds the subscript tag to the page, so it appears at end of line
-  range.insertNode(sup);
+  //tag the content as a comment--to be used to prevent recommenting
+  var comment = document.createElement("commented");
 
-  //adds the old text to the page.
-  range.insertNode(colored);
+  comment.appendChild(colored);
+  comment.appendChild(sup);
 
-
-
+  range.insertNode(comment);
 }
-
-
 
 
 //Used to send function calls from the extension
@@ -103,19 +100,81 @@ chrome.runtime.onMessage.addListener(
         //console.log("Otherinfo is"+request.other);
         var url = unique(request.url);
         restoreData(url);
+        sendResponse({farewell:"goodbye"});
         return;
       }
       else if(request.greeting === "highlight")
       {
         hflag = true;
+        sendResponse({farewell:"goodbye"});
         return;
         //highlight();
+      }
+      else if(request.greeting === "colors")
+      {
+        setColor(request.setting.high, request.setting.comment);
+        sendResponse({farewell:"goodbye"});
+        return;
       }
       console.log(request.greeting);
       flag = true;
       comText = request.greeting;
     //}
   });
+
+function restoreColors()
+{
+  chrome.storage.local.get("boxcolor", function (data) { 
+
+    if(!data["boxcolor"])
+    {
+      console.log("No saved box preferences.")
+    }
+    console.log("box color is currently "+data['boxcolor']);
+    boxColor = data["boxcolor"];
+    
+    
+    
+  });
+
+
+  chrome.storage.local.get("highcolor", function (data) { 
+   if(!data["highcolor"])
+   {
+    console.log("No saved highlight preferences.");
+    return;
+  }
+  console.log("High color is currently "+data['highcolor']);
+  highColor = data["highcolor"];
+
+});
+
+  console.log("Current colors are b="+boxColor+" h="+highColor);
+}
+
+
+function setColor(hColor, bColor)
+{
+    switch(hColor){
+      case "yellow" : highColor = "yellow"; break;
+      case "blue" : highColor = "#98d5ff"; break;
+      case "green" : highColor = "#2ecc71"; break;
+    }
+
+    switch(bColor){
+      case "grey" : boxColor = "rgba(237,238,238,0.8)"; break;
+      case "clear" : boxColor = "rgba(255,0,0,0.008)"; break;
+      case "red" : boxColor = "#fcc"; break;
+    }
+
+
+
+    var obj = {};
+    obj["boxcolor"] = boxColor;
+    obj["highcolor"] = highColor;
+    chrome.storage.local.set(obj);
+}
+
 
 function createTitle()
 {
@@ -149,6 +208,10 @@ function getFullDate()
     h = h-12;
     noon = "PM";
   } 
+  if(min<10)
+  {
+    min = "0"+min;
+  }
   var timeString = "("+m+"/"+d+"/"+y+" "+h+":"+min+noon+")";
   //console.log("Timestring is "+timeString);
   return timeString;
@@ -159,138 +222,118 @@ function getFullDate()
 function highlight()
 {
     var temp = window.getSelection();
-    var selected = window.getSelection().toString();
+    //var tempNode = window.getSelection().extentNode.parentNode;
+
+    //var selected = window.getSelection().toString();
     range = temp.getRangeAt(0);
     if(range.collapsed)
     {
       console.log("Range is collapsed.");
       return;
     }
+    var tempNode = range.cloneContents();
+    //console.log("temp is "+tempNode);
 
     range.deleteContents();
   
   //creates an element to change the text color.
   var colored = document.createElement("font");
-  colored.style.backgroundColor = "yellow";
+  //colored.style.backgroundColor = "yellow";
+  colored.style.backgroundColor = highColor;
 
   //create a text node for the old text
-  var oldText = document.createTextNode(selected);
-  colored.appendChild(oldText);
+  //var oldText = document.createTextNode(selected);
+  //var oldText = document.createTextNode(temp);
+
+  colored.appendChild(tempNode);
   //oldText.style.backgroundColor = "red";
   //adds the old text to the page.
   range.insertNode(colored);
   hflag = false;
 }
 
-
-
 function commentText(commentText)
 {
-    var temp = window.getSelection();
-    var selected = window.getSelection().toString();
+  var temp = window.getSelection();
+  var selected = window.getSelection().toString();
+  //locateText(selected);
+  range = temp.getRangeAt(0);
+  if(range.collapsed)
+  {
+    console.log("Range is collapsed.");
+    return;
+  }
+  var rangeCopy = range.cloneContents();
+  var mydiv = document.createElement('div'); 
 
-    //locateText(selected);
-    range = temp.getRangeAt(0);
-    if(range.collapsed)
-    {
-      console.log("Range is collapsed.");
-        return;
-    }
+  //I am using this function to get a greater consistent context.
+  //If I used the selection to append the comment, then it would be.
+  var superNode = getSuperNode();
+  superNode.appendChild(mydiv);
 
-    commentNum = commentNum + 1;
-    
-    replaceText(selected,temp,range);
-    var mydiv = document.createElement('div'); 
-    var ts = getFullDate();
-    //this sets the text for the comments.
-    mydiv.innerText=commentNum+". "+ts+" "+commentText;
-    mydiv.id = commentNum+"note";
-    //added for fixing nested highlights
-    //mydiv.className = 'notethat-noted';
-    mydiv.style.fontSize="15px";
-    selectionNode = window.getSelection().extentNode.parentNode;
-    /* Try to append to end of "paragraphs" instead of highlights
-    and comments
-    */
-    /*console.log("Class is "+temp.className);
-    if (temp.className === 'notethat-noted') {
-      selectionNode = selectionNode.parentNode;
-    }*/
-    
-    /*if(commentNum===1)
-    {
-      titleDiv = createTitle();
-      selectionNode.offsetParent.appendChild(titleDiv);
+  console.log(superNode.innerHTML);
 
-    }*/
-    
-    //selectionNode.offsetParent.appendChild(mydiv);
+  //Check if they're trying to comment already commented text
+  /*console.log(superNode.getElementsByTagName("sup").length);
+  if(superNode.getElementsByTagName("sup").length > 0) {
+    return;
+  }
+*//*
+  if(superNode.innerHTML.indexOf("sup")!=-1)
+  {
+    console.log("Break out");
+    return;
+  }*/
+  console.log(rangeCopy);
+  if(rangeCopy.getElementsByTagName("commented").length>0)
+  {
+    console.log("Breaking free");
+    return;
+  }
+  // Scopey iisues
+ /* if (superNode.getElementsByTagName("commented").length > 0) {
+    return;
+  }*/
 
-    selectionNode.appendChild(mydiv);
+  commentNum = commentNum + 1;
+  
+  replaceText(selected,temp,range);
 
-
-    mydiv.style.position = "static";
-    mydiv.style.left = selectionNode.offsetLeft + "px";
-    mydiv.style.top = selectionNode.offsetTop + selectionNode.offsetHeight + "px";
-    //mydiv.style.width = "700px";
-    //mydiv.style.display = "block";
-    //mydiv.style.float = "right";
-    //mydiv.style.cssFloat = "right";
-    mydiv.style.border = "4px solid #d00";
-    mydiv.style.background = "#fcc";
+  var ts = getFullDate();
+  //this sets the text for the comments.
+  mydiv.innerText=commentNum+". "+ts+" "+commentText;
+  mydiv.id = commentNum+"note";
+  mydiv.style.fontSize="15px";
 
 
-    //Code to store data for saving later
-    var offset = document.getElementById(commentNum+"note").getBoundingClientRect();
-    /*var l = offset.left;
-    var t = offset.top;
-    var text = mydiv.innerText;
-    var tempid = commentNum;
-    var refText = selected;
-    */
-    
-
-    console.log("Top is "+offset.top+" left is "+offset.left);
-    flag = false;
+  mydiv.style.position = "static";
+  mydiv.style.left = superNode.offsetLeft + "px";
+  mydiv.style.top = superNode.offsetTop + superNode.offsetHeight + "px";
+  //mydiv.style.width = "700px";
+  //mydiv.style.display = "block";
+  //mydiv.style.float = "right";
+  //mydiv.style.cssFloat = "right";
+  mydiv.style.padding = "0px 8px";
+  mydiv.style.margin = "4px 0px";
+  mydiv.style.border = "2px solid #d00";
+  mydiv.style.background = boxColor;
+  //Code to store data for saving later
+  var offset = document.getElementById(commentNum+"note").getBoundingClientRect();
+  
+  flag = false;
 }
 
 
 function restoreData(url)
 {
   console.log("in restoreData url is "+url);
-  chrome.storage.sync.get(url, function (data) { 
+  chrome.storage.local.get(url, function (data) { 
     if(!data[url])
     {
       console.log("No saved data!");
       return;
     }
-    console.log("data is " +data[url].divs[0].comment);
-    console.log(data);
-    //var pageData = data;
-
-    //console.log(pageData);
-    console.log("Now debugging object");
-    for(var key in data.divs)
-    {
-      var obj = data[key];
-      for(var prop in obj)
-      {
-        if(obj.hasOwnProperty(prop)){
-          console.log(prop+" = "+obj[prop]);
-        }
-      }
-    }
-
-    /*var mydiv = document.createElement('div');
-    mydiv.innerText = pageData[0].comment;
-    mydiv.id = pageData[0].id+"note";
-    mydiv.style.fontsize = "15px";
-    mydiv.style.position = "static";
-    mydiv.style.left = pageData[0].left;
-    mydiv.style.top = pageData[0].top;
-    mydiv.style.background = "#fcc";*/
-    //pageData[0].parent.appendChild(mydiv);
-
+    document.documentElement.innerHTML = data[url].webpage;
   });
 }
 
@@ -304,78 +347,46 @@ function unique(urlstring)
 }
 
 
-function record()
-{
-
-}
-
-//Need to fix this function.
 function savePage(tabUrl)
 {
   console.log("inside of savePage "+tabUrl);
-    
-    for (var i = 1; i <= commentNum; i++) {
-      var div = document.getElementById(i+"note");
-      var offset = div.getBoundingClientRect();
-      var l = offset.left;
-      var t = offset.top;
-      var text = div.innerText;
-      var tempid = i;
-      var parentNode = div.offsetParent.id;
-      savedDivs[savedDivs.length] = {left:l,top:t,comment:text, id : tempid, parent:parentNode };
+  var pageData ={};
+  // Save the content of the webpage
+  pageData.webpage = document.documentElement.innerHTML;
+  pageData.url = unique(tabUrl);
+  
+  console.log("before save url is "+tabUrl);
+  var obj = {};
+  obj[tabUrl] = pageData;
+  chrome.storage.local.set(obj);
 
-      //console.log(tempObject); 
-    }
-    pageData.divs = savedDivs;
-    pageData.url = unique(tabUrl); 
-    
-    console.log("before save url is "+tabUrl);
-    var obj = {};
-    obj[tabUrl] = pageData;
-    chrome.storage.sync.set(obj, function () {
-        console.log("Just stored ",pageData);
-    });
-
-
-    /*for (var i = 0; i<=savedDivs.length-1; i++)
-    {
-        console.log(savedDivs[i]);
-    }*/
-    //console.log(pageData);
-    return pageData;
+  return pageData;
 }
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
-  var button = document.getElementById("button")
-  console.log("Button Was Clicked");
-  button.addEventListener('click', click);
+  console.log("Dom content loaded.");
+  
 });
 
-// Move that bubble to the appropriate location.
-function renderBubble(mouseX, mouseY, selection) {
-  bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
-}
-
-
-function locateText(str)
+// Gets the selected text's node, its parent's, or its ancestor's.
+function getSuperNode()
 {
-  var flag = true;
-  position = 0;
-
-  /*while(flag === true)
-  {
-    if(position === -1)
-    {
-      flag=false;
-    }
-    var position = document.documentElement.innerHTML.indexOf(str,position);
-    console.log("The position is "+position);
-  }*/
-  
-}
-
-
+  var node,selection;
+  if (window.getSelection) {
+    selection = getSelection();
+    node = selection.anchorNode;
+  }
+  if (!node && document.selection) {
+      selection = document.selection
+      var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+      node = range.commonAncestorContainer ? range.commonAncestorContainer :
+             range.parentElement ? range.parentElement() : range.item(0);
+  }
+  if (node) {
+    return node.parentNode;
+  }
+};
 
